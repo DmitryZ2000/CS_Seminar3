443828a3765cad27cad07f0d7adb83340af25044
DVZ <orionvega5@gmail.com>
2022-08-11 21:46:20 +0300

    Add Task022

---
+++ Task022/Program.cs
@@ -0,0 +1,13 @@
+﻿void PowerN(int number)
+{
+    int index = 1;
+    while(index <= number)
+    {
+        Console.Write($"{index*index} ");
+        index++;
+    }
+}
+
+Console.Write("Введите любое целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+PowerN(num);
