f3787fd4e7830d33c336cff9eb40c42092339b1a
DVZ <orionvega5@gmail.com>
2022-08-11 21:10:56 +0300

    add task018

---
+++ Task_function/Program.cs
@@ -0,0 +1,18 @@
+﻿int FindQuarter(int x, int y)
+{
+    if (x>0 && y>0)
+        return 1;
+    else if (x<0 && y>0)
+        return 2;
+    else if (x<0 && y<0)
+        return 3;
+    else if (x>0 && y<0)
+        return 2;
+    return 0;
+}
+
+Console.WriteLine(FindQuarter(0,4));
+Console.WriteLine(FindQuarter(1,4));
+Console.WriteLine(FindQuarter(-2,4));
+Console.WriteLine(FindQuarter(-3,-4));
+Console.WriteLine(FindQuarter(2,-4));
