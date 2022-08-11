397d546a4afcfe089cd6aa1c48768fb1a18c5711
DVZ <orionvega5@gmail.com>
2022-08-11 21:10:00 +0300

    Add task018

---
+++ Task018/Program.cs
@@ -0,0 +1,17 @@
+﻿void Quater(int number)
+{
+    if (number >= 1 && number <= 4)
+        if (number == 1)
+            Console.WriteLine("Это первая четверть и x > 0 и y > 0");
+        else if (number == 2)
+            Console.WriteLine("Это вторая четверть и x > 0 и y < 0");
+        else if (number == 3)
+            Console.WriteLine("Это третья четверть и x < 0 и y < 0");
+        else if (number == 4)
+            Console.WriteLine("Это четверть четверть и x > 0 и y < 0");
+    else
+        Console.WriteLine("Не верно введена номер четверти");
+
+}
+
+Quater(1);
